01c12dc62f333a577e75432ae7c32b82da64a3cf
ulugbek9728 <ulugbekshukurov9728@gmail.com>
2022-01-19 11:21:55 +0500

    create puma-amaliy

---
+++ js/index.js
@@ -0,0 +1,41 @@
+$('#CarouselOne').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    dots:false,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:2
+        }
+    }
+})
+
+$('#CarouselTwo').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    dots:false,
+    autoplayTimeout:4000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+})
